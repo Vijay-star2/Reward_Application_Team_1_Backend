1cc5af240244f215bfed2a5cba32d46ab3c61c6c
Vijay-star1 <vijaykumar.anigeni@tcs.com>
2025-12-26 10:34:47 +0000

    Add Maven Spring project files

---
+++ Reward-Redemption-Application/src/main/java/com/tcs/Reward/Redemption/Application/RewardRedemptionApplication.java
@@ -0,0 +1,13 @@
+package com.tcs.Reward.Redemption.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RewardRedemptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RewardRedemptionApplication.class, args);
+	}
+
+}
---
+++ Reward-Redemption-Application/src/test/java/com/tcs/Reward/Redemption/Application/RewardRedemptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcs.Reward.Redemption.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardRedemptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
